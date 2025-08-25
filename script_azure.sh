8e2dca8a12fd459412d767b0c008ff07cb5689d2
kevinbillepro <kevin.bille.pro@gmail.com>
2025-08-25 09:51:29 +0200

    Update script_azure.sh

--- script_azure.sh
+++ script_azure.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-# Ce script récupère les coûts réels sur la subscription active
-# Prérequis : az login déjà effectué
-
+# Usage: ./get_costs.sh <subscription_id>
 SUBSCRIPTION_ID=$1
 az account set --subscription $SUBSCRIPTION_ID
 
-# Récupérer les coûts des 30 derniers jours par ressource
+# Récupération des coûts des 30 derniers jours par ressource
 az costmanagement query \
-    --type Usage \
-    --timeframe MonthToDate \
-    --dataset-aggregation TotalCost=Sum \
-    --dataset-grouping Type=Dimension Name=ResourceId \
-    --query "[].{Resource:properties/resourceId, Cost:properties/totalCost}" \
-    -o json
+  --type Usage \
+  --timeframe MonthToDate \
+  --dataset-aggregation TotalCost=Sum \
+  --dataset-grouping Type=Dimension Name=ResourceId \
+  --query "[].{Resource:properties/resourceId, Cost:properties/totalCost}" \
+  -o json
